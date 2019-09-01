beae73c0c6a0bd06e55454fc78a6820b17769236
Andrey <yastrebsov@icloud.com>
2019-09-01 17:05:11 +0300

    travis

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,28 +1,28 @@
-       var mainNav = document.querySelector('.site-navigation');
-        var logo = document.querySelector ('.site-logo');
-        var navOpener = document.querySelector('.site-header__burger');
-        var navCloser = document.querySelector('.site-header__closer');
+var mainNav = document.querySelector('.site-navigation');
+var logo = document.querySelector ('.site-logo');
+var navOpener = document.querySelector('.site-header__burger');
+var navCloser = document.querySelector('.site-header__closer');
 
-        mainNav.classList.add('site-navigation__js-active');
+mainNav.classList.add('site-navigation__js-active');
 
-        navOpener.classList.remove ('site-header__burger--js-active');
+navOpener.classList.remove ('site-header__burger--js-active');
 
-        logo.classList.add ('site-logo--js-active');
+logo.classList.add ('site-logo--js-active');
 
-        navCloser.classList.remove ('site-header__closer--js-active');
+navCloser.classList.remove ('site-header__closer--js-active');
 
-        navOpener.addEventListener('click',function() {
-          mainNav.classList.remove('site-navigation__js-active');
+navOpener.addEventListener('click',function() {
+  mainNav.classList.remove('site-navigation__js-active');
 
-          navOpener.classList.add('site-header__burger--js-active');
+  navOpener.classList.add('site-header__burger--js-active');
 
-          logo.classList.remove ('site-logo--js-active');
-        });
+  logo.classList.remove ('site-logo--js-active');
+});
 
-        navCloser.addEventListener('click',function() {
-          mainNav.classList.add('site-navigation__js-active');
+navCloser.addEventListener('click',function() {
+  mainNav.classList.add('site-navigation__js-active');
 
-          navOpener.classList.remove('site-header__burger--js-active');
+  navOpener.classList.remove('site-header__burger--js-active');
 
-          logo.classList.add ('site-logo--js-active');
-        });
+  logo.classList.add ('site-logo--js-active');
+});
